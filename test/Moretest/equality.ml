fd6980b5ba1a6d63c074a57322f4b55eb442a178
No author <no_author@ocaml.org>
2000-10-02 14:18:06 +0000

    This commit was manufactured by cvs2svn to create branch 'poly_meth'.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/poly_meth@3305 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- test/Moretest/equality.ml
+++
@@ -1,8 +0,0 @@
-let x = [1;2;3]
-
-let f x = 1 :: 2 :: 3 :: x
-
-let _ =
-  if x = f [] then print_string "OK " else print_string "failed! ";
-  if stdout <> stderr then print_string "OK" else print_string "failed!";
-  print_newline()
