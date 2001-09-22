8cb382bee35271cbd7161166b10111ce36993294
No author <no_author@ocaml.org>
2001-09-22 12:06:17 +0000

    This commit was manufactured by cvs2svn to create branch 'variants303'.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/variants303@3776 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- camlp4/tools/ocamlc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-if test "`basename $OTOP`" != "ocaml_stuff"; then
-    COMM="$OTOP/boot/ocamlrun$EXE $OTOP/boot/ocamlc -I $OTOP/boot"
-else
-    COMM=ocamlc$OPT
-fi
-echo $COMM $*
-$COMM $*
