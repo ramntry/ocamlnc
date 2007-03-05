6b9bb83c10d127d59d36a378d34fa641f6e75cdf
Alain Frisch <alain@frisch.fr>
2007-03-05 21:08:26 +0000

    *** empty log message ***
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/natdynlink@7999 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- test/natdynlink/factorial.c
+++ test/natdynlink/factorial.c
@@ -1,9 +1,15 @@
 #include "caml/mlvalues.h"
+#include "caml/memory.h"
+#include "caml/alloc.h"
 
 value factorial(value n){
+  CAMLparam1(n);
+  CAMLlocal2(a,b);
+
   int m = Int_val(n);
   int x = 1;
   int i;
   for (i = 1; i <= m; i++) x *= i;
-  return (Val_int(x));
+  a = copy_string("abc");
+  CAMLreturn (Val_int(x));
 }
