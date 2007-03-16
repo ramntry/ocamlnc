c4f61a58ad4a2a2e684b364ffc1539bf056c225a
Alain Frisch <alain@frisch.fr>
2007-03-16 11:08:27 +0000

    sync with release310
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/natdynlink@8087 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- build/boot.sh
+++ build/boot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# $Id$
 cd `dirname $0`/..
 set -ex
 ./boot/ocamlrun boot/myocamlbuild.boot boot/stdlib.cma boot/std_exit.cmo
@@ -6,7 +7,7 @@ boot/ocamlrun boot/myocamlbuild.boot -log _boot_log1 \
   ocamlbuild/ocamlbuildlightlib.cma ocamlbuild/ocamlbuildlight.byte
 rm -f _build/myocamlbuild
 boot/ocamlrun boot/myocamlbuild.boot \
-  -just-plugin -install-dir _build/ocamlbuild -byte-plugin
+  -just-plugin -install-lib-dir _build/ocamlbuild -byte-plugin
 cp _build/myocamlbuild boot/myocamlbuild
 ./boot/ocamlrun boot/myocamlbuild $@ -log _boot_log2 \
   boot/camlheader ocamlc
