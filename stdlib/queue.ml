a274609d65083d08659e9d082a7f53feb041aecb
Alain Frisch <alain@frisch.fr>
2012-03-12 17:43:37 +0000

    Revert bad commit.
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/constructors_with_record@12220 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- stdlib/queue.ml
+++ stdlib/queue.ml
@@ -21,9 +21,10 @@ exception Empty
    cyclic lists, so as to eliminate the [Nil] case and the sum
    type. *)
 
-type 'a cell =
-  | Cons of {content: 'a; mutable next: 'a cell}
-  | Nil
+type 'a cell = {
+    content: 'a;
+    mutable next: 'a cell
+  }
 
 (* A queue is a reference to either nothing or some cell of a cyclic
    list. By convention, that cell is to be viewed as the last cell in
@@ -45,49 +46,49 @@ type 'a t = {
 
 let create () = {
   length = 0;
-  tail = Nil;
+  tail = Obj.magic None
 }
 
 let clear q =
   q.length <- 0;
-  q.tail <- Nil
+  q.tail <- Obj.magic None
 
 let add x q =
-  match q.tail with
-  | Nil ->
-      let rec cell =
-        Cons
-          {
-           content = x;
-           next = cell
-          }
-      in
-      q.length <- 1;
-      q.tail <- cell
-  | Cons cons ->
-      let head = cons.next in
-      let cell = Cons {content = x; next = head} in
-      q.length <- q.length + 1;
-      cons.next <- cell;
-      q.tail <- cell
+  if q.length = 0 then
+    let rec cell = {
+      content = x;
+      next = cell
+    } in
+    q.length <- 1;
+    q.tail <- cell
+  else
+    let tail = q.tail in
+    let head = tail.next in
+    let cell = {
+      content = x;
+      next = head
+    } in
+    q.length <- q.length + 1;
+    tail.next <- cell;
+    q.tail <- cell
 
 let push =
   add
 
 let peek q =
-  match q.tail with
-  | Cons {next = Cons{content}} -> content
-  | _ -> raise Empty
+  if q.length = 0 then
+    raise Empty
+  else
+    q.tail.next.content
 
 let top =
   peek
 
 let take q =
-  match q.tail with
-  | Nil -> raise Empty
-  | Cons cons ->
-      q.length <- q.length - 1;
-      let head = tail.next in
+  if q.length = 0 then raise Empty;
+  q.length <- q.length - 1;
+  let tail = q.tail in
+  let head = tail.next in
   if head == tail then
     q.tail <- Obj.magic None
   else
