f790a4e83a9f252a1ce702247327dc2bd45e0f35
ramntry <ramntry@gmail.com>
2013-11-30 20:43:50 +0400

    fix bug with autotest in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+cwd=`pwd`
+
 ./run_configure.sh || exit 1
 make world.opt || exit 2
 make install || exit 4
 cp -r scripts $OCAML_ROOT/bin || exit 8
 ./build_dummy_gc.sh || exit 16
-./gc/tests/trees/check.sh || exit 32
+
+cd $cwd/gc/tests/trees
+./check.sh || exit 32
+cd $cwd
 
 exit 0
