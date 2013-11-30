4c7ecbd3f01e62440cd076ad70fae94d804e6657
ramntry <ramntry@gmail.com>
2013-12-01 03:30:45 +0400

    fix accidental changes in boot directory end setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,9 +1,9 @@
 export CC="gcc"
 export CXX="g++"
-export OCAML_ROOT=/home/ramntry/projects/ocaml/ocaml/testbuild/dist
-export LLVM_ROOT=/opt/llvm/3.3/with_ocaml_bindings_again_build
-export LLVM_SRC_ROOT=/home/ramntry/projects/ocaml/llvm/3.2/llvm
-export LLVM_OBJ_ROOT=/home/ramntry/projects/ocaml/llvm/3.2/build_with_ocaml_bindings_again
+export OCAML_ROOT="PLACE OCAML INSTALLATION DIRECTORY (FULL PATH) HERE (e.g. /home/user/ocamlnc/dist)"
+export LLVM_ROOT="PLACE LLVM INSTALLATION DIRECTORY (FULL PATH) HERE (e.g. /home/user/ocamlnc/llvm/dist)"
+export LLVM_SRC_ROOT="PLACE LLVM SOURCE DIRECTORY (FULL PATH) HERE (e.g. /home/user/ocamlnc/llvm/llvm)"
+export LLVM_OBJ_ROOT="PLACE LLVM BUILD DIRECTORY (FULL PATH) HERE (e.g. /home/user/ocamlnc/llvm/build)"
 export JBLABGC="$PWD/gc/jblab-gc-dummy/build/Release+Asserts/lib/jblab-gc-dummy.so"
 
 export OCAML_INCLUDE="$OCAML_ROOT/lib/ocaml"
