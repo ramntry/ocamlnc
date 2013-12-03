1e25ad050f5ed8235cf976eaf485567b70f67291
ramntry <ramntry@gmail.com>
2013-12-03 22:16:53 +0400

    nt2 script was introduced. Tests 'integer_arithmetic', 'function_call', 'let_var_bindings' in basic section were added and passed

---
+++ testsuite/ocamlnc/basic/integer_arithmetic/integer_arithmetic.ml
@@ -0,0 +1,8 @@
+external print_endline : string -> unit = "caml_print_endline";;
+external print_string : string -> unit = "caml_print_string";;
+external read_float : unit -> float = "caml_read_float";;
+external print_float : float -> unit = "caml_print_float";;
+external read_int : unit -> int = "caml_read_int";;
+external print_int : int -> unit = "caml_print_int";;
+
+print_int (((10 + 5) / 4 * 7) mod 20);;
