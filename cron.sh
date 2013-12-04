acdbac971d1d753aee912457396a848ec850e62c
Roman Tereshin <roman.y.tereshin@gmail.com>
2013-12-04 11:27:43 -0500

    fix bug with full paths in extract_test_log srcript

--- cron.sh
+++ cron.sh
@@ -3,6 +3,6 @@
 BASEDIR=/root/ocamlnc
 
 $BASEDIR/bs.sh > $BASEDIR/bs.stdout.log.running 2> $BASEDIR/bs.stderr.log.running
-cat bs.stdout.log.running >> bs.stdout.log
-cat bs.stderr.log.running >> bs.stderr.log
-./extract_test_log bs.stdout.log.running | mail -s '*** ocamlnc build server log ***' ramntry@gmail.com
+cat $BASEDIR/bs.stdout.log.running >> $BASEDIR/bs.stdout.log
+cat $BASEDIR/bs.stderr.log.running >> $BASEDIR/bs.stderr.log
+$BASEDIR/extract_test_log $BASEDIR/bs.stdout.log.running | mail -s 'ocamlnc build server log' ramntry@gmail.com
