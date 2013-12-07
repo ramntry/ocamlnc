17d713db8676a35324bbe510af5c6395e5b14d31
ramntry <ramntry@gmail.com>
2013-12-08 02:57:07 +0400

    tests about evaluation order was added

---
+++ testsuite/ocamlnc/basic/evaluation_order/evaluation_order.ml
@@ -0,0 +1,23 @@
+external make_vect : int -> 'a -> 'a array = "caml_make_vect";;
+external print_endline : string -> unit = "caml_print_endline";;
+external print_string : string -> unit = "caml_print_string";;
+external read_float : unit -> float = "caml_read_float";;
+external print_float : float -> unit = "caml_print_float";;
+external read_int : unit -> int = "caml_read_int";;
+external print_int : int -> unit = "caml_print_int";;
+external print_char : char -> unit = "caml_print_char";;
+external create_string : int -> string = "caml_create_string";;
+external blit_string : string -> int -> string -> int -> int -> unit = "caml_blit_string";;
+
+let println_int n =
+  print_int n;
+  print_endline ""
+
+let f a b c d =
+  println_int a;
+  println_int b;
+  println_int c;
+  println_int d
+
+let () =
+  f (read_int ()) (read_int ()) (read_int ()) (read_int ())
